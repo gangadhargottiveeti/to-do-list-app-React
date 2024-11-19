1a459079978c7b4c83cd1d6a84a03b2a71ef8342
Gangadhar Gottiveeti <gangadhar9398645347@gmail.com>
2024-11-19 10:44:39 +0530

    Add. more data for todos

--- src/data/todos.js
+++ src/data/todos.js
@@ -3,5 +3,7 @@ export const todos = [
     {id : 2, text : "Complete assignments", completed : false},
     {id : 3, text : "Prepare breakfast", completed : false},
     {id : 4, text : "Sleep for 2 hours", completed : false},
-    {id : 5, text : "Take a shower", completed : false}
+    {id : 5, text : "Take a shower", completed : false},
+    {id : 6, text : "Walk for 1km", completed : false},
+    {id : 7, text : "Watch a movie", completed : false}
   ]
